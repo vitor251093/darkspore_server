a097a5a30b3a69e89fe64e9cb943dc0fb200c34f
Vitor <vitor251093@gmail.com>
2020-09-20 17:22:21 -0300

    Removing unecessary falgs

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 /usr/src/mxe/usr/bin/x86_64-w64-mingw32.shared-cmake . \
-     -DMAKE_PREFIX_PATH="/work" \
-     -DBOOST_ROOT="/work/libs/boost_1_70_0" \
-     -DBOOST_LIBRARYDIR="/work/libs/boost_1_70_0"
+     -DCMAKE_BUILD_TYPE=Release
 
 make
