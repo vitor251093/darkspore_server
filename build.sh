29ef84c69d5fa3a24f2a0fdc7eefb412633b36e4
Vitor <vitor251093@gmail.com>
2020-09-20 15:15:32 -0300

    Starting the support for Docker building

---
+++ build.sh
@@ -0,0 +1,6 @@
+/usr/src/mxe/usr/bin/x86_64-w64-mingw32.shared-cmake . \
+     -DMAKE_PREFIX_PATH="/work" \
+     -DBOOST_ROOT="/work/libs/boost_1_70_0" \
+     -DBOOST_LIBRARYDIR="/work/libs/boost_1_70_0"
+
+make
---
+++ prepare_docker.sh
@@ -0,0 +1,4 @@
+docker run --rm dockcross/windows-shared-x64 > ./dockcross-windows-shared-x64
+chmod +x ./dockcross-windows-shared-x64 
+./dockcross-windows-shared-x64 bash -c './build.sh'
+
