2e9669f86747131d6aff22998b3728bbabe93f80
Vitor <vitor251093@gmail.com>
2020-09-27 17:20:31 -0300

    rapidjson is now building in CMake

--- prepare_docker.sh
+++ prepare_docker.sh
@@ -1,4 +1,2 @@
 docker run --rm dockcross/windows-shared-x64 > ./dockcross-windows-shared-x64
-chmod +x ./dockcross-windows-shared-x64 
-./dockcross-windows-shared-x64 bash -c './build.sh'
 
---
+++ run_docker.sh
@@ -0,0 +1,2 @@
+chmod +x ./dockcross-windows-shared-x64 
+./dockcross-windows-shared-x64 bash -c './build.sh'
