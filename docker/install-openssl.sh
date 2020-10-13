538cbe22617e5b8c08f670eef8cd1bcf4810806c
Vitor <vitor251093@gmail.com>
2020-10-12 21:27:40 -0300

    Trying to fix the OpenSSL build process

--- docker/install-openssl.sh
+++ docker/install-openssl.sh
@@ -1,8 +1,18 @@
+# References
+# https://www.howtoforge.com/tutorial/how-to-install-openssl-from-source-on-linux/
+
+apt update
+apt install build-essential checkinstall zlib1g-dev -y
+
+cd /usr/local/src/
 wget https://www.openssl.org/source/openssl-1.1.1b.tar.gz
-mkdir /opt/openssl
-tar xfvz openssl-1.1.1b.tar.gz --directory /opt/openssl
-#export LD_LIBRARY_PATH=/opt/openssl/lib
-cd /opt/openssl/openssl-1.1.1b
-./config --prefix=/opt/openssl --openssldir=/opt/openssl/ssl
+tar -xf openssl-1.1.1b.tar.gz
+cd openssl-1.1.1b
+./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl -DOPENSSL_SYS_WIN32 shared zlib
 make
+make test
 make install
+
+cd /etc/ld.so.conf.d/
+cat "/usr/local/ssl/lib" >> ./openssl-1.0.2o.conf
+ldconfig -v
