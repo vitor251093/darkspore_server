a99757109bcbc7ca2a30792911b0319ddc3fcfaf
Vitor <vitor251093@gmail.com>
2020-10-18 21:05:02 -0300

    CMake is being installed now

--- cmake_run.sh
+++ cmake_run.sh
@@ -1,6 +1,6 @@
 cd "${0%/*}"
 
-/usr/src/mxe/usr/bin/x86_64-w64-mingw32.shared-cmake . \
+cmake . \
      -DCMAKE_BUILD_TYPE=Release \
      -DPROGRAMFILES=C:\\Program\ Files \
      -DPROGRAMFILESX86=C:\\Program\ Files\ \(x86\)
